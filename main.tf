efeab23fecbcf86c9b6ad53f3f09462b06028c16
kalynar <34781398+kalynar@users.noreply.github.com>
2022-10-04 15:43:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                = "kalynakeyvault2"
+  name                = "kalynakeyvault"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
